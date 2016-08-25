0ebb4041bdc10835cd931bddfa66972f9c5d8624
sunjj <sjjman@163.com>
2016-08-25 08:48:38 +0800

    ios手势解锁样式调整

--- klinsurance-app/html/index/guesture.js
+++ klinsurance-app/html/index/guesture.js
@@ -3,36 +3,38 @@ var rightCode = '';//退出次数
 apiready = function () {
     //双击返回退出
     exitApp();
-    if(ht.storage.getLocalStorage(ht.constants.rightCode)){
+    if (ht.storage.getLocalStorage(ht.constants.rightCode)) {
         rightCode = ht.storage.getLocalStorage(ht.constants.rightCode);
     }
     var mode = 0;
-    if(!rightCode){
+    if (!rightCode) {
         $("#title").html('请设置手势密码');
         mode = 1;
     }
+    var left = (api.winWidth - 320) / 2;
+    var top = api.winWidth * 0.44;
     var patternLock = api.require('patternLock');
     patternLock.addPatternLock({
         viewName: '/html/index/guesture.html',
         rightColor: '#00FF00',
         drawColor: '#222222',
         wrongColor: '#FF0000',
-        normalColor: '#000000',
-        frame: '{{0,100},{320,300}}',
+        normalColor: '#FFFFFF',
+        frame: '{{'+left+','+top+'},{320,300}}',
         mode: mode,
         rightCode: rightCode
-    }, function(ret, err) {
+    }, function (ret, err) {
         if (ret) {
-            if(ret.status ==0) {
+            if (ret.status == 0) {
                 goIndex();
-            }else if(ret.status ==1){
+            } else if (ret.status == 1) {
                 $("#title").html('手势密码错误');
-            }else if(ret.status ==2){
+            } else if (ret.status == 2) {
                 $("#title").html('请再次输入手势密码');
-            }else if(ret.status ==3){
-                $("#title").html('两次输入不一致，请再次输入');
-            }else if(ret.status ==4){
-                ht.storage.setLocalStorage(ht.constants.rightCode,ret.code);
+            } else if (ret.status == 3) {
+                $("#title").html('两次输入不一致，请重新输入');
+            } else if (ret.status == 4) {
+                ht.storage.setLocalStorage(ht.constants.rightCode, ret.code);
                 goIndex();
             }
         } else {
@@ -53,9 +55,9 @@ function exitApp() {
                 duration: 2000,
                 location: 'bottom'
             });
-            setTimeout(function(){
+            setTimeout(function () {
                 exitNum = 0;
-            },2000)
+            }, 2000)
         } else {
             api.closeWidget({
                 id: api.appId,  //填写自己的id
@@ -71,6 +73,6 @@ function goIndex() {
         name: '/html/index/index_win.html',
         url: 'index_win.html',
         bounces: false,
-        slidBackEnabled:false
+        slidBackEnabled: false
     });
 }
