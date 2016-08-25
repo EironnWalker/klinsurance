8384655a60b974c8fcc5f36a09ce8454e23eff46
sunjj <sjjman@163.com>
2016-08-25 16:44:13 +0800

    登陆调整

--- klinsurance-app/html/sync/info_sync_frm.js
+++ klinsurance-app/html/sync/info_sync_frm.js
@@ -1,3 +1,4 @@
+var db;
 var userId;
 var lastSyncInfoObj = {};
 apiready = function () {
@@ -11,8 +12,66 @@ apiready = function () {
         $("#lastSyncTime").html("暂未同步，请同步数据");
         $("#lastSyncContent").html("暂未同步，请同步数据");
     }
+    if (lastSyncInfoObj != null) {
+
+    }
 };
+
+function setBriefingListStr() {
+    db.selectSql({
+        name: 'klinsurance_db.db',
+        sql: "SELECT * FROM crm_customer where type = " + custType + " and user_id = '" + userId + "'"
+    }, function (ret, err) {
+        if (ret.status) {
+
+        } else {
+            api.toast({
+                msg: '数据错误，请稍后重试！',
+                duration: 2000,
+                location: 'bottom'
+            });
+        }
+    });
+}
+
+
 //数据同步
 function syncData() {
-
+    api.ajax({
+        url: ht.ip.insuranceservice + '/projectSyncLogAction/getCanSyncMessage',
+        method: 'post',
+        timeout: 30,
+        dataType: 'json',
+        returnAll: false,
+        data: {
+            values: {
+                userId: userId,
+                lastSyncTime: lastSyncInfoObj == null ? null : lastSyncInfoObj.createTime,
+                briefingListStr: userId,
+                briefingLossListStr: userId,
+                briefingLossImageListStr: userId,
+                briefingLossItemListStr: userId,
+                customerListStr: userId,
+                lossListStr: userId,
+                lossItemListStr: userId,
+                reportListStr: userId,
+                reportBriefingListStr: userId,
+                textTemplateListStr: userId
+            }
+        }
+    }, function (ret, err) {
+        if (ret) {
+            if (ret && ret.success) {
+                ht.storage.setLocalStorage(ht.constants.isFirstCust, 0);
+                //插入本地数据并更新远程数据
+                addLocalCustAndUpdateRemote(ret.data);
+            }
+        } else {
+            api.toast({
+                msg: '网络错误，请稍后重试！',
+                duration: 2000,
+                location: 'bottom'
+            });
+        }
+    });
 }
