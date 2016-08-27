a76c939294cc7ebf0a5171fa4cb2d6b97a796a2d
feicy <feichenyun@126.com>
2016-08-27 17:26:01 +0800

    修复发送邮件常量配置

---
+++ klinsurance-service/src/main/java/com/ht/klinsurance/utils/email/Test.java
@@ -0,0 +1,12 @@
+package com.ht.klinsurance.utils.email;
+
+/**
+ * @author feicy
+ * @date 2016/8/27
+ */
+public class Test {
+
+    public static void main(String[] args) throws Exception{
+        EmailUtils.sendMail("测试邮件","liuqi@haitao-tech.com","294546283@qq.com","测试邮件","");
+    }
+}
