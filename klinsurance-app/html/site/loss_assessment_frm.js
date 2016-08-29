337c198ddca91f5169db41ad8ea7f2d129504192
feicy <feichenyun@126.com>
2016-08-29 16:31:06 +0800

    完成添加损失项功能

--- klinsurance-app/html/site/loss_assessment_frm.js
+++ klinsurance-app/html/site/loss_assessment_frm.js
@@ -1,35 +1,49 @@
-var CustomDesignModule = null;
-var imgSrc;
+var pageParam;
+var searchContent;
 $(function () {
     $("li .flex-1").each(function () {
         if ($(this).height() > 20) {
             $(this).removeClass("text-right");
         }
     })
+    $(".ul-one").click(function() {
+        api.openWin({
+            name: '/html/site/add_loss_win.html',
+            url: api.wgtRootDir + '/html/site/add_loss_win.html',
+            pageParam: {name: 'pageparamname'}
+        });
+    });
 });
 
 apiready = function () {
-    CustomDesignModule = api.require('CustomDesignModule');
+    pageParam=api.pageParam;
+    loadData();
 };
-
-
-$(".zoom").click(
-    function () {
-        startDesigh();
+// 加载数据
+function loadData() {
+    var sql = "SELECT * FROM loss_item where lossId='"+pageParam.lossId+"'";
+    if (searchContent) {
+        sql += " and lossItemName like '%" + searchContent + "%'";
     }
-);
-
-function startDesigh() {
-    CustomDesignModule.startDesigh(function back(ret) {
-        alert(ret.data)
-        $(".sign img").attr("src", ret.data)
-    });
+    db.selectSql({
+            name: 'klinsurance_db.db',
+            sql: sql
+        }, function (ret, err) {
+            if (ret.status) {
+                var html = template('template', ret);
+                $("#content").html(html);
+            } else {
+                api.toast({
+                    msg: '数据错误，请稍后重试！',
+                    duration: 2000,
+                    location: 'bottom'
+                });
+            }
+            //将刷新完成后关闭
+            api.refreshHeaderLoadDone();
+            ht.apicloud.hideProgress();
+        }
+    );
 }
 
-$(".ul-one").click(function() {
-    api.openWin({
-        name: '/html/site/add_loss_win.html',
-        url: api.wgtRootDir + '/html/site/add_loss_win.html',
-        pageParam: {name: 'pageparamname'}
-    });
-});
+
