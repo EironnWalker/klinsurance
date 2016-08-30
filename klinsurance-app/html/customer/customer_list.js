7ba850bc92ca3f0312e201380833812e231979e6
sunjj <sjjman@163.com>
2016-08-30 11:35:47 +0800

    添加简报

--- klinsurance-app/html/customer/customer_list.js
+++ klinsurance-app/html/customer/customer_list.js
@@ -1,9 +1,10 @@
 var db;
 var yH;//打开通讯录列表的起始高度
-var viewADBook;
 var TranslateModule = null;
 var importCustomModule = null;
-var bookType;
+var searchContent;//搜索内容
+var bookType;//类型 0 user 1 customer
+var fromPage;//从哪个页面过来的，然后选择完成后回调哪个页面的方法
 apiready = function () {
     var headerHeight = $("header").fixStatusBar();
     TranslateModule = api.require('TranslateModule');
@@ -11,16 +12,31 @@ apiready = function () {
     db = api.require('db');
     yH = headerHeight + $(".horizontal").outerHeight(true);
     bookType = api.pageParam.bookType;
-    firstRefresh();
+    fromPage = api.pageParam.fromPage;
+    $("#title").html(api.pageParam.title);
+    refresh();
 };
 //进入页面首次刷新数据
-function firstRefresh() {
+function refresh() {
+    var sql = "";
+    if (bookType == 1) {
+        if (searchContent) {
+            sql = "SELECT * FROM customer where name like '%" + searchContent + "%'";
+        } else {
+            sql = "SELECT * FROM customer";
+        }
+    } else {
+        if (searchContent) {
+            sql = "SELECT * FROM user where name like '%" + searchContent + "%'";
+        } else {
+            sql = "SELECT * FROM user";
+        }
+    }
     db.selectSql({
         name: 'klinsurance_db.db',
-        sql: "SELECT * FROM customer"
+        sql: sql
     }, function (ret, err) {
         if (ret.status) {
-            alert(JSON.stringify(ret.data));
             initCustomerBook(ret.data);
         } else {
             api.toast({
@@ -39,11 +55,29 @@ function initCustomerBook(data) {
         w: 0,
         h: api.winHeight - yH,
         type: bookType,
-        persons: JSON.stringify(data)
+        data: JSON.stringify(data)
     };
     importCustomModule.importCustom(param, function resultCallback(ret) {
-        alert(ret.data);
+        var jsfun;
+        if (fromPage == "brief") {
+            if (bookType == 1) {
+                jsfun = 'setBBR(' + ret.data + ');';
+            } else {
+                jsfun = 'setGGR(' + ret.data + ');';
+            }
+            api.execScript({
+                name: '/html/report/add_brief_win1.html',
+                frameName: '/html/report/add_brief_frm1.html',
+                script: jsfun
+            });
+        }
+        api.closeWin();
     });
+}
+
+function searchCustomer(){
+    searchContent = $("#search").val();
+    refresh();
 }
 $(".icon-record").click(
     function () {
